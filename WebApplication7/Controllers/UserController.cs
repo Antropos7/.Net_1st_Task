68e3b6fa25d2a75008632899bb0fb00254f684f6
Antropos7 <ruslanshm@code.edu.az>
2023-05-24 18:38:58 +0400

    first commit

---
+++ WebApplication7/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication7.Models;
+
+namespace WebApplication7.Controllers
+{
+   
+        public class UserController : Controller
+        {
+            public ActionResult Index()
+            {
+                ViewBag.Name = "Ruslan";
+                ViewBag.Surname = "Murtuzaliyev";
+                ViewBag.Age = 23;
+                ViewBag.Country = "Azerbaijan";
+                ViewBag.PhoneNumber = "+994504413450";
+
+                return View();
+            }
+        }
+    
+}
